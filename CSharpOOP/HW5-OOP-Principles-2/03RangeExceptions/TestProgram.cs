f11f3106d5f40513b4810076182bf7186d28b14e
gbelcheva <gbelcheva@users.noreply.github.com>
2015-03-29 02:58:31 +0200

    Update TestProgram.cs

--- CSharpOOP/HW5-OOP-Principles-2/03RangeExceptions/TestProgram.cs
+++ CSharpOOP/HW5-OOP-Principles-2/03RangeExceptions/TestProgram.cs
@@ -13,12 +13,12 @@
             catch (InvalidRangeException<int> ex)
             {
                 Console.WriteLine(ex);
-                throw new InvalidRangeException<DateTime>("Invalid dates range", new DateTime(1981, 1, 1), new DateTime(2013, 12, 31));
+                throw new InvalidRangeException<DateTime>("Invalid dates range", new DateTime(1980, 1, 1), new DateTime(2013, 12, 31));
             }
 
 
             //throw new InvalidRangeException<int>("Invalid number range", 1, 100);
-            //throw new InvalidRangeException<DateTime>("Invalid dates range", new DateTime(1981, 1, 1), new DateTime(2013, 12, 31));
+            //throw new InvalidRangeException<DateTime>("Invalid dates range", new DateTime(1980, 1, 1), new DateTime(2013, 12, 31));
         }
     }
 }
