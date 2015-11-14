e81c035122480a8f7a6a968bca51f1f1dbb694c7
AdrianApostolov <adrian.apostolov.1991@gmail.com>
2015-11-14 15:41:11 +0200

    Added flirts(likes) functionality

---
+++ DateFirst.Api/Controllers/FlirtsController.cs
@@ -0,0 +1,53 @@
+﻿namespace DateFirst.Api.Controllers
+{
+    using System.Web.Http;
+    using DateFirst.Api.DataTransferModels;
+    using DateFirst.Data.Repositories;
+    using System.Net.Http;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
+
+    public class FlirtsController : ApiController
+    {
+        private readonly IDateFirstData data;
+
+        public FlirtsController(IDateFirstData data)
+        {
+            this.data = data;
+
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var userManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var user = userManager.FindByName(User.Identity.Name);
+
+            var currentUser = this.data.Users.GetById(user.Id);
+            return this.Ok(currentUser.Flirts);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put([FromBody]UserTransferModel user)
+        {
+            var userManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            if (User.Identity.Name == null)
+            {
+                return this.BadRequest("You must login!");
+            }
+            var currentLoginUser = userManager.FindByName(User.Identity.Name);
+
+            if (currentLoginUser.Id == user.Id)
+            {
+                return this.BadRequest("You cannot flirts your on profile!");
+            }
+
+            var currentProfilUser = this.data.Users.GetById(user.Id);
+            currentProfilUser.Flirts += 1;
+            this.data.Users.Update(currentProfilUser);
+            this.data.SaveChanges();
+
+            return this.Ok();
+        }
+    }
+}
--- DateFirst.Web/Scripts/Controllers/usersController.js
+++ DateFirst.Web/Scripts/Controllers/usersController.js
@@ -22,13 +22,37 @@
             user = resUser;
             return templates.get('userProfile');
         })
-        .then(function (template) {
-            context.$element().html(template(user));
-            $(".btn-pref .btn").click(function () {
-                $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-                $(this).removeClass("btn-default").addClass("btn-primary");
+            .then(function (template) {
+                context.$element().html(template(user));
+                $(".btn-pref .btn").click(function () {
+                    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+                    $(this).removeClass("btn-default").addClass("btn-primary");
+                });
+
+                $('#flirts').on('click', function () {
+                    $.ajax({
+                        type: "PUT",
+                        data: user,
+                        url: "http://localhost:9941//api/Flirts",
+                        headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') },
+                        error: function(xhr, status, error) {
+                            var err = eval("(" + xhr.responseText + ")");
+                            toastr.error(err.Message);
+                        }
+                    })
+                    .then(function () {
+                        toastr.success('You flirted this profile!');
+                        $.ajax({
+                            type: "GET",
+                            url: "http://localhost:9941/api/UserProfiles/" + user.Id,
+                            headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }
+                        })
+                            .then(function (res) {
+                                $('#count').text(res.Flirts);
+                            });
+                    });
+                });
             });
-        })
     }
 
     function editUserProfilePage(context) {
@@ -39,67 +63,66 @@
             user = resUser;
             return templates.get('editProfile');
         })
-        .then(function (template) {
-            context.$element().html(template(user));
-            if (user.AdditionalInfo != null) {
-                var currentGender = user.AdditionalInfo.Gender;
-                var currentEyeColor = user.AdditionalInfo.EyeColor;
-                var currentHairColor = user.AdditionalInfo.HairColor;
-                var currentStarSign = user.AdditionalInfo.StarSign;
-                var currentAge = user.AdditionalInfo.Age;
-                var currentHeight = user.AdditionalInfo.Heigth;
-                var currentWeight = user.AdditionalInfo.Weight;
-                var currendJobTitle = user.AdditionalInfo.JobTitle;
-                var currentSalary = user.AdditionalInfo.Salary;
-                var currentAboutMe = user.AdditionalInfo.AboutMe;
-            }
-
-            $('#select-gender').val(currentGender);
-            $('#select-eye-color').val(currentEyeColor);
-            $('#select-hair-color').val(currentHairColor);
-            $('#select-star-sign').val(currentStarSign);
-            $('#tb-age').val(currentAge);
-            $('#tb-height').val(currentHeight);
-            $('#tb-weight').val(currentWeight);
-            $('#tb-job-title').val(currendJobTitle);
-            $('#tb-salary').val(currentSalary);
-            $('#ta-about-me').val(currentAboutMe);
-
-            $("#btn-save-changes").click(function myfunction() {
-
-                var modifiedGender = $('#select-gender').val();
-                var modifiedEyeColor = $('#select-eye-color').val();
-                var modifiedHairColor = $('#select-hair-color').val();
-                var modifiedStarSign = $('#select-star-sign').val();
-                var modifiedAge = $('#tb-age').val();
-                var modifiedHeight = $('#tb-height').val();
-                var modifiedWeight = $('#tb-weight').val();
-                var modifiedJobTitle = $('#tb-job-title').val();
-                var modifiedSalary = $('#tb-salary').val();
-                var modifiedAboutMe = $('#ta-about-me').val();
-
-                var data = {
-                    'Id': user.Id,
-                    'Gender': modifiedGender,
-                    'EyeColor': modifiedEyeColor,
-                    'HairColor': modifiedHairColor,
-                    'StarSign': modifiedStarSign,
-                    'Age': modifiedAge,
-                    'Heigth': modifiedHeight,
-                    'Weight': modifiedWeight,
-                    'JobTitle': modifiedJobTitle,
-                    'Salary': modifiedSalary,
-                    'AboutMe': modifiedAboutMe
+            .then(function (template) {
+                context.$element().html(template(user));
+                if (user.AdditionalInfo != null) {
+                    var currentGender = user.AdditionalInfo.Gender;
+                    var currentEyeColor = user.AdditionalInfo.EyeColor;
+                    var currentHairColor = user.AdditionalInfo.HairColor;
+                    var currentStarSign = user.AdditionalInfo.StarSign;
+                    var currentAge = user.AdditionalInfo.Age;
+                    var currentHeight = user.AdditionalInfo.Heigth;
+                    var currentWeight = user.AdditionalInfo.Weight;
+                    var currendJobTitle = user.AdditionalInfo.JobTitle;
+                    var currentSalary = user.AdditionalInfo.Salary;
+                    var currentAboutMe = user.AdditionalInfo.AboutMe;
                 }
 
-                userModel.updateUserInfo(data)
-                    .then(function (res) {
-                        
-                        document.location = '/#/users/' + user.Id + '/my-profile';
-                    });
-            })
-        })
+                $('#select-gender').val(currentGender);
+                $('#select-eye-color').val(currentEyeColor);
+                $('#select-hair-color').val(currentHairColor);
+                $('#select-star-sign').val(currentStarSign);
+                $('#tb-age').val(currentAge);
+                $('#tb-height').val(currentHeight);
+                $('#tb-weight').val(currentWeight);
+                $('#tb-job-title').val(currendJobTitle);
+                $('#tb-salary').val(currentSalary);
+                $('#ta-about-me').val(currentAboutMe);
 
+                $("#btn-save-changes").click(function myfunction() {
+
+                    var modifiedGender = $('#select-gender').val();
+                    var modifiedEyeColor = $('#select-eye-color').val();
+                    var modifiedHairColor = $('#select-hair-color').val();
+                    var modifiedStarSign = $('#select-star-sign').val();
+                    var modifiedAge = $('#tb-age').val();
+                    var modifiedHeight = $('#tb-height').val();
+                    var modifiedWeight = $('#tb-weight').val();
+                    var modifiedJobTitle = $('#tb-job-title').val();
+                    var modifiedSalary = $('#tb-salary').val();
+                    var modifiedAboutMe = $('#ta-about-me').val();
+
+                    var data = {
+                        'Id': user.Id,
+                        'Gender': modifiedGender,
+                        'EyeColor': modifiedEyeColor,
+                        'HairColor': modifiedHairColor,
+                        'StarSign': modifiedStarSign,
+                        'Age': modifiedAge,
+                        'Heigth': modifiedHeight,
+                        'Weight': modifiedWeight,
+                        'JobTitle': modifiedJobTitle,
+                        'Salary': modifiedSalary,
+                        'AboutMe': modifiedAboutMe
+                    }
+
+                    userModel.updateUserInfo(data)
+                        .then(function (res) {
+
+                            document.location = '/#/users/' + user.Id + '/my-profile';
+                        });
+                });
+            });
     }
 
     scope.users = {
