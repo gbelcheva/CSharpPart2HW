b6c136264ce3d66b08dcf7f1401b2f382b78c3a6
ggarabedian <garabedian2001@gmail.com>
2015-11-15 16:23:41 +0200

    Added Shout Box UI. Needs visual glitch fix.

--- DateFirst.Web/Scripts/Controllers/uploadFileController.js
+++ DateFirst.Web/Scripts/Controllers/uploadFileController.js
@@ -1,27 +1,27 @@
-﻿(function () {
-    $(document).ready(function () {
-        $('#btnUploadFile').on('click', function () {
-            var data = new FormData();
+﻿//(function () {
+//    $(document).ready(function () {
+//        $('#btnUploadFile').on('click', function () {
+//            var data = new FormData();
 
-            var files = $("#fileUpload").get(0).files;
+//            var files = $("#fileUpload").get(0).files;
 
-            if (files.length > 0) {
-                data.append("UploadedImage", files[0]);
-                console.log(files[0]);
-            }
+//            if (files.length > 0) {
+//                data.append("UploadedImage", files[0]);
+//                console.log(files[0]);
+//            }
 
-            var ajaxRequest = $.ajax({
-                type: "POST",
-                url: "http://localhost:9941//api/FileUpload",
-                contentType: false,
-                processData: false,
-                data: data,
-                headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }
-            });
+//            var ajaxRequest = $.ajax({
+//                type: "POST",
+//                url: "http://localhost:9941//api/FileUpload",
+//                contentType: false,
+//                processData: false,
+//                data: data,
+//                headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }
+//            });
 
-            ajaxRequest.done(function (xhr, textStatus) {
-                // Do other operation
-            });
-        });
-    });
-}());
+//            ajaxRequest.done(function (xhr, textStatus) {
+//                // Do other operation
+//            });
+//        });
+//    });
+//}());
