c3e8385f017ee83d9f7ec32af398ff8989d4920a
ggarabedian <garabedian2001@gmail.com>
2015-11-15 11:14:53 +0200

    Upload file button location changed

--- DateFirst.Web/Scripts/Controllers/usersController.js
+++ DateFirst.Web/Scripts/Controllers/usersController.js
@@ -112,6 +112,31 @@
         })
         .then(function (template) {
             context.$element().html(template(user));
+
+            $('#btnUploadFile').on('click', function () {
+                var data = new FormData();
+
+                var files = $("#fileUpload").get(0).files;
+
+                if (files.length > 0) {
+                    data.append("UploadedImage", files[0]);
+                    console.log(files[0]);
+                }
+
+                var ajaxRequest = $.ajax({
+                    type: "POST",
+                    url: "http://localhost:9941//api/FileUpload",
+                    contentType: false,
+                    processData: false,
+                    data: data,
+                    headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }
+                });
+
+                ajaxRequest.done(function (xhr, textStatus) {
+                    // Do other operation
+                });
+            });
+
             if (user.AdditionalInfo != null) {
                 var currentGender = user.AdditionalInfo.Gender;
                 var currentEyeColor = user.AdditionalInfo.EyeColor;
