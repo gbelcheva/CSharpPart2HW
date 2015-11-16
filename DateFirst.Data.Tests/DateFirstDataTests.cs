8295bc975cbe9e151355bc179745e86a22daf9ba
V.Dinev <vassildinev@gmail.com>
2015-11-16 17:17:10 +0200

    Added DateFirstData tests

---
+++ DateFirst.Data.Tests/DateFirstDataTests.cs
@@ -0,0 +1,34 @@
+﻿namespace DateFirst.Data.Tests
+{
+    using Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using Repositories;
+    using TestObjects;
+
+    [TestClass]
+    public class DateFirstDataTests
+    {
+        private static IDateFirstData mockedDataObject;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            IRepository<TestEntity> mockedRepositoryObject = new Mock<IRepository<TestEntity>>().Object;
+            var mockedData = new Mock<IDateFirstData>();
+            mockedData.Setup(x => x.GetRepository<TestEntity>())
+                .Returns(mockedRepositoryObject);
+            mockedDataObject = mockedData.Object;
+        }
+
+        /// <summary>
+        /// Only this method is being tested since it is invoked for every repository
+        /// </summary>
+        [TestMethod]
+        public void GetEntityShouldNotReturnNull()
+        {
+            IRepository<TestEntity> repository = mockedDataObject.GetRepository<TestEntity>();
+            Assert.AreNotEqual(null, repository);
+        }
+    }
+}
